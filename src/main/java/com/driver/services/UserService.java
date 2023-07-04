7a5976e5aab20827e803beaad8611ec71b2d3119
Greeny Don <greenydon198@gmail.com>
2023-07-04 12:42:17 +0530

    3

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -30,13 +30,13 @@ public class UserService {
         return user.getId();
     }
 
-    public Integer getAvailableCountOfWebSeriesViewable(Integer userId) throws Exception {
+    public Integer getAvailableCountOfWebSeriesViewable(Integer userId)  {
 
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
         Optional<User> userOptional = userRepository.findById(userId);
-        if(!userOptional.isPresent())
-            throw new Exception("User Not Found.");
+//        if(!userOptional.isPresent())
+//            throw new Exception("User Not Found.");
 
         User user = userOptional.get();
         SubscriptionType type = user.getSubscription().getSubscriptionType();
